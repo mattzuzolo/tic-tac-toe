d772b3c5e230edfc65878303ddbbf04b293fae14
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-07 16:30:19 -0500

    user can now click on a spot to play a turn. Marker is visually placed and current user switches

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 console.log("JS is connected.");
 
-const turnCounter = 0;
+let turnCounter = 0;
 const tileArray = [...document.getElementsByClassName("div--tile")];
 
-console.log("whose turn:", xTurn(turnCounter))
+console.log("X's turn?", isItXTurn(turnCounter))
 
 //https://stackoverflow.com/questions/21616393/javascript-event-listener-firing-on-page-load-not-click-event
 tileArray.map(tile => {
+
+  //Displays and hides current user's marker as they choose a spot on the board
   tile.addEventListener("mouseenter", displayMarker.bind(event));
   tile.addEventListener("mouseleave", removeMarker.bind(event)); 
+
+  tile.addEventListener("click", playTurn.bind(event));
 });
 
-function xTurn(turnCounter){
+function playTurn(event){
+  const tile = event.target;
+  tile.className += " played";
+  tile.firstChild.innerText = currentMarker();
+  event.target.style.color = "red";
+  turnCounter++;
+}
+
+
+
+
+
+function isItXTurn(turnCounter){
   if (turnCounter % 2){
     return false;
   } else {
@@ -19,22 +35,33 @@ function xTurn(turnCounter){
   }
 }
 
-function displayMarker(event){
-  const tile = event.target;
-
-  if(xTurn(turnCounter)){
-    console.log("tile", tile.firstChild);
-    tile.firstChild.innerText = "X";
+function currentMarker(){
+  if(isItXTurn(turnCounter)){
+    return "X";
   } 
   else {
-    tile.firstChild.innerText = "O";
+    return "O";
   } 
-  event.target.style.color = "red";
+}
+
+function displayMarker(event){
+  const tile = event.target;
+  if (tile.className.includes("played")) return console.log("Spot taken");
+
+
+  if(isItXTurn(turnCounter)){
+    tile.firstChild.innerText = currentMarker();
+  } 
+  else {
+    tile.firstChild.innerText = currentMarker();
+  } 
+  event.target.style.color = "gray";
 }
 
 function removeMarker(event){
   const tile = event.target;
-
-  console.log("Left the tile");
-  event.target.style.color = "transparent";
+  if (tile.className.includes("played")){
+    return console.log("Someone already used that tile");
+  }
+  tile.style.color = "transparent";
 }
