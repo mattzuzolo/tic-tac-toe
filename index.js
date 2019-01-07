2af556e1862f8e2c9db1760a85fc9455f175c63a
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-07 17:07:36 -0500

    Marker now working on hover and click. Working to remove event listeners when game is over

--- index.js
+++ index.js
@@ -6,21 +6,33 @@ const tileArray = [...document.getElementsByClassName("div--tile")];
 console.log("X's turn?", isItXTurn(turnCounter))
 
 //https://stackoverflow.com/questions/21616393/javascript-event-listener-firing-on-page-load-not-click-event
-tileArray.map(tile => {
 
-  //Displays and hides current user's marker as they choose a spot on the board
-  tile.addEventListener("mouseenter", displayMarker.bind(event));
-  tile.addEventListener("mouseleave", removeMarker.bind(event)); 
 
-  tile.addEventListener("click", playTurn.bind(event));
-});
+document.addEventListener("DOMContentLoaded", () => {
+  tileArray.map(tile => {
+
+    //Displays and hides current user's marker as they choose a spot on the board
+    tile.addEventListener("mouseenter", displayMarker.bind(event));
+    tile.addEventListener("mouseleave", removeMarker.bind(event)); 
+  
+    tile.addEventListener("click", playTurn.bind(event));
+    console.log("Original instance", playTurn);
+  });
+})
 
 function playTurn(event){
   const tile = event.target;
-  tile.className += " played";
-  tile.firstChild.innerText = currentMarker();
-  event.target.style.color = "red";
-  turnCounter++;
+  if (!tile.className.includes("played")){
+    tile.className += " played";
+    tile.firstChild.innerText = currentMarker();
+    event.target.style.color = "red";
+    turnCounter++;
+    checkGameOver()
+    console.log("Current turn:", turnCounter);
+  } else {
+    console.log("That spot is taken!");
+  }
+  
 }
 
 
@@ -46,22 +58,33 @@ function currentMarker(){
 
 function displayMarker(event){
   const tile = event.target;
-  if (tile.className.includes("played")) return console.log("Spot taken");
 
-
-  if(isItXTurn(turnCounter)){
+  if (!tile.className.includes("played")){
     tile.firstChild.innerText = currentMarker();
-  } 
-  else {
-    tile.firstChild.innerText = currentMarker();
-  } 
-  event.target.style.color = "gray";
+    event.target.style.color = "gray";
+  } else {
+    console.log("That spot is already taken!")
+  }
 }
 
 function removeMarker(event){
   const tile = event.target;
-  if (tile.className.includes("played")){
-    return console.log("Someone already used that tile");
+  if (!tile.className.includes("played")){
+    return tile.style.color = "transparent";
   }
-  tile.style.color = "transparent";
+}
+
+function checkGameOver(){
+  if (turnCounter >= 9){
+    alert("Game over!")
+    tileArray.map(tile => {
+      tile.removeEventListener("mouseenter", displayMarker.apply(event));
+      tile.removeEventListener("mouseleave", removeMarker.apply(event));
+      tile.removeEventListener("click", playTurn(event));
+    
+    
+    });
+
+
+  }
 }
